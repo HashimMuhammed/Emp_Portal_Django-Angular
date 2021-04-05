82ffb364284394f4b4b6f8829c22c2bbbfbb7bef
unknown <hashimmuhammed555@gmail.com>
2021-04-05 21:36:14 +0530

    show-department

--- frontend/frontend/src/app/department/add-dep/add-dep.component.ts
+++ frontend/frontend/src/app/department/add-dep/add-dep.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Input, OnInit } from '@angular/core';
+import {SharedService} from "src/app/shared.service"
 @Component({
   selector: 'app-add-dep',
   templateUrl: './add-dep.component.html',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDepComponent implements OnInit {
 
-  constructor() { }
+  constructor( private service:SharedService) { }
+@Input() dep:any
+DepartmentId:any
+DepartmentName:any
 
   ngOnInit(): void {
+    this.DepartmentId=this.dep.DepartmentId;
+    this.DepartmentName=this.dep.DepartmentName
+  }
+   addDepartment(){
+    var val = {DepartmentId:this.DepartmentId,
+                DepartmentName:this.DepartmentName};
+    this.service.addDep(val).subscribe(res=>{
+      alert(res.toString());
+    });
+  }
+  updateDepartment(){
+    var val = {DepartmentId:this.DepartmentId,
+      DepartmentName:this.DepartmentName};
+this.service.updateDep(val).subscribe(res=>{
+alert(res.toString());
+});
   }
 
 }
--- frontend/frontend/src/app/department/show-dep/show-dep.component.ts
+++ frontend/frontend/src/app/department/show-dep/show-dep.component.ts
@@ -7,15 +7,40 @@ import {SharedService} from 'src/app/shared.service';
   styleUrls: ['./show-dep.component.css']
 })
 export class ShowDepComponent implements OnInit {
-  depList:any=[]
+ 
   constructor(private service:SharedService) { }
+  depList:any=[];
+  ModalTitle: any;
+  ActivateAddDepComp:boolean=false;
+  dep:any;
+
+  addClick(){
+    this.dep={
+      DepartmentId:0,
+      DepartmentName:""
+    }
+    this.ModalTitle="Add Department"
+    this.ActivateAddDepComp=true;
+  }
+  closeClick(){
+    this.ActivateAddDepComp=false
+    this.refreshDepList()
+  }
+  editClick(item:any){
+    this.dep=item;
+    this.ModalTitle="Edit Department"
+    this.ActivateAddDepComp=true;
+
+  }
+
 
   ngOnInit(): void {
     this.refreshDepList();
   }
+  
 refreshDepList(){
   this.service.getDepList().subscribe(data=>{
-    this.depList=data
+    this.depList=data;
   });
 }
 
